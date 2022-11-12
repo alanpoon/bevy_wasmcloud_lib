49d7e17ca6d491ceddd9292aeffc9c37b57cd3c3
alanpoon <brightshadowx@gmail.com>
2022-11-12 21:12:54 +0800

    move to crates

--- crates/client-websocket/src/lib.rs
+++ crates/client-websocket/src/lib.rs
@@ -4,9 +4,9 @@ mod native;
 #[cfg(not(target_arch = "wasm32"))]
 pub use native::*;
 
-#[cfg(target_arch = "wasm32")]
+//#[cfg(target_arch = "wasm32")]
 mod wasm;
-#[cfg(target_arch = "wasm32")]
+//#[cfg(target_arch = "wasm32")]
 pub use wasm::*;
 
 mod next_vec;
